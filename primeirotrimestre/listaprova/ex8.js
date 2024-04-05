a18fb42dd881e8463ea626fec7dc297d2c2ab093
Rayovaac <160878772+Rayovaac@users.noreply.github.com>
2024-04-04 21:10:47 -0300

    Update ex8.js

--- primeirotrimestre/listaprova/ex8.js
+++ primeirotrimestre/listaprova/ex8.js
@@ -1,1 +1,16 @@
+if (lado1==0 || lado2==0 || lado3==0){
+ window.alert ("Não é um triângulo. ")
+}
+ else{
+ if (lado1==lado2 && lado2==lado3) {
+ window.alert ("0 triângulo é equilátero. ")
+ }
+ else if (lado1==lado2 || lado2==lado3 || lado1==lado3){
+ window.alert ("0 triângulo é isósceles. ")
+ }
+ else{
+ window.alert ("0 triângulo é escaleno. ")
+  }
+   
+ }
 
